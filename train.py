f63987f880cdb297a0f4981af58b3980983db1bb
dominik335 <dominiksabat@o2.pl>
2018-07-13 02:42:10 +0200

    change to training

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+#from __future__ import print_function
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.layers import GRU
