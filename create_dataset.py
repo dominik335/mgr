aee16094df7134d03bf1d83913e4401519a92905
dominik335 <dominiksabat@o2.pl>
2018-07-13 02:03:19 +0200

    s

--- create_dataset.py
+++ create_dataset.py
@@ -6,19 +6,19 @@ from methods import *
 
 no_features = 60
 values = np.array([]).reshape(0, no_features)
-
-i = 0
+np.del
+i,j = 0
 indir = '/home/dsabat/blog/classical/'
 for root, dirs, filenames in os.walk(indir):
     for f in filenames:
         if i % 40 == 0:
             print(i)
         i = 1 + i
-        if (i==100):
-            break
         values = np.vstack((values, convert(indir + f)))
 
-        if values.size == 0:
-            continue
+        if (values.shape[0]>=200000):
+            np.save("/home/dsabat/datasety/" + str(j), values)
+            j=j+1
+            values=None
+            values = np.array([]).reshape(0, no_features)
 
-np.save("dataset_b_cla.npy",values)
