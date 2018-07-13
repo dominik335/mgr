f85a8c86acd916e9ea4e55b3f206ea43b4c44fc3
dominik335 <dominiksabat@o2.pl>
2018-07-13 02:09:36 +0200

    correct convert

--- methods.py
+++ methods.py
@@ -80,7 +80,7 @@ def convert(path):
     midi_data = pretty_midi.PrettyMIDI(path)
     roll = midi_data.get_piano_roll(fs=timeres)
     roll= np.where(roll>0 ,1,-1)
-    while(np.all(roll[:,0] == 0)): #drop leading "0" columns
+    while(np.all(roll[:,0] == -1)): #drop leading "0" columns
         roll = np.delete(roll,0,1)
     return np.transpose(roll[40:100]) #pitch LtR, time UtD
 
