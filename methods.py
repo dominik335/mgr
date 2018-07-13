c89e95799a930add6982983f82ca1b0864b122fc
dominik335 <dominiksabat@o2.pl>
2018-07-13 02:44:59 +0200

    change to training

--- methods.py
+++ methods.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import pretty_midi
 
 
--- train.py
+++ train.py
@@ -10,6 +10,7 @@ from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
 
+from methods import *
 #misc
 import numpy as np
 import os
